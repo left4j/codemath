fe654b877c53a8b9d620ad5d65342fdae7a310a0
조케손유 <52685674+left4j@users.noreply.github.com>
2020-05-01 19:31:40 +0200

    Update af.cpp

--- Ackermann function/af.cpp
+++ Ackermann function/af.cpp
@@ -30,9 +30,9 @@ int main()
     cout << "Enter n: ";
     cin >> k;
     cout << "Won't compute for A(4,2) and higher" << endl;
-    // the longest int available in C++ is unsigned long long int which is only 2^64-2, while A(4,2) is 2^65533-3,
+    // the longest int available in C++ is unsigned long long int which is only 2^64-2, while A(4,2) is already 2^65533-3,
     // even boost library only allows for int1024_t, which is also just 2^1024-1, so even if stack overflow 
-    // doesn't occur, cout will spit out a very wrong result, therefore it's better to leave A(m,n) as a simple int 
+    // doesn't occur before, cout will spit out a very wrong result, therefore it's better to leave A(m,n) as a simple int 
     // that will fail to compute for A(4,2) and anything above, but at least won't return an incorrect answer
     int AF;
     AF = A(j,k);
