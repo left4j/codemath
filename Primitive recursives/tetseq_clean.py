f0300a13ab0df79d4f25807b5c2dae2077d3e250
조케손유 <52685674+left4j@users.noreply.github.com>
2020-05-06 19:02:42 +0200

    indentation fix

--- Primitive recursives/tetseq_clean.py
+++ Primitive recursives/tetseq_clean.py
@@ -15,15 +15,15 @@ k = int(c)
 H = hyperop(4)
 s1 = 0
 y = int(input('\n'"Input y: "))
-    for f in range(f,k+1):
-        j = H(f,f+1)
-        s1 += j
+for f in range(f,k+1):
+    j = H(f,f+1)
+    s1 += j
 s2 = H(s1,y+1)
-        t = str(s2-1)
-        o = str(s2)+t
-        p = int(o)
-        V = 0
-        for s2 in range (s2,p+1):
-            e = (H(s2,s2+1))
-            V += e
+    t = str(s2-1)
+    o = str(s2)+t
+    p = int(o)
+    V = 0
+    for s2 in range (s2,p+1):
+        e = (H(s2,s2+1))
+        V += e
 print('\n' "V(f,y) = ", V)
