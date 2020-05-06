78cb32b71c8a6041066ae9d690e665bd30e143e3
조케손유 <52685674+left4j@users.noreply.github.com>
2020-05-06 19:27:19 +0200

    variables cleanup

--- Primitive recursives/tetseq_clean.py
+++ Primitive recursives/tetseq_clean.py
@@ -16,14 +16,12 @@ H = hyperop(4)
 s1 = 0
 y = int(input('\n'"Input y: "))
 for f in range(f,k+1):
-    j = H(f,f+1)
-    s1 += j
+   s1 = H(f,f+1)
 s2 = H(s1,y+1)
     t = str(s2-1)
     o = str(s2)+t
     p = int(o)
     V = 0
     for s2 in range (s2,p+1):
-        e = (H(s2,s2+1))
-        V += e
+        V +=( H(s2,s2+1))
 print('\n' "V(f,y) = ", V)
