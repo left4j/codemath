005db71d54793e8374a490b694bb6797a0e6286a
조케손유 <52685674+left4j@users.noreply.github.com>
2020-05-07 15:50:30 +0200

    add code

--- Collatz Conjecture/CC.py
+++ Collatz Conjecture/CC.py
@@ -1,1 +1,18 @@
-
+n = int(input('\n'"Insert a number: "))
+if n%2:
+    print("Is odd")
+else:
+    print("Is even")
+print("________________")
+print("Starting..."'\n')
+w = 0
+while n != 1:
+    if n%2 == 0:
+        n = n//2
+        print (n)
+    else:
+        n = 3*n+1
+        print (n)
+    w = w + 1
+if n == 1:
+        print('\n'"Returned to 1. Took", w, "steps to terminate, try again")
