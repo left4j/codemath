04c185dd7c9d90ff33b20dc7b61b522013cd03a6
조케손유 <52685674+left4j@users.noreply.github.com>
2020-05-09 00:44:26 +0200

    auf wiedersehen print

--- Ackermann function/Ackermann function.py
+++ Ackermann function/Ackermann function.py
@@ -7,7 +7,6 @@ Kinda useless, will just leave here for whatever's sake
 '''
 j = int(input('\n'"Enter m: "))
 k = int(input("Enter n: "))
-print('\n'"A(4,2) and above will not work, unless ran on a supercomputer")
 def A(m,n):
     if m==0:
         return n+1
@@ -15,5 +14,4 @@ def A(m,n):
         return A(m-1,1)
     elif m>0 and n>0:
         return A(m-1,A(m,n-1))
-
-print('\n'"Result:", A(j,k))
+print('\n'"A(4,2) and above will not work, unless ran on a supercomputer"'\n'"Result:", A(j,k))
