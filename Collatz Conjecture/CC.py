30c8ae3a542f03ecfee49c1aea78351492f95074
조케손유 <52685674+left4j@users.noreply.github.com>
2020-05-09 00:40:27 +0200

    respect indentation, get rid of 1 print

--- Collatz Conjecture/CC.py
+++ Collatz Conjecture/CC.py
@@ -3,8 +3,7 @@ if n%2:
     print("Is odd")
 else:
     print("Is even")
-print("________________")
-print("Starting..."'\n')
+print("________________"'\n'"Starting..."'\n')
 w = 0
 while n != 1:
     if n%2 == 0:
@@ -15,4 +14,4 @@ while n != 1:
         print (n)
     w = w + 1
 if n == 1:
-        print('\n'"Returned to 1. Took", w, "steps to terminate, try again")
+    print('\n'"Returned to 1. Took", w, "steps to terminate, try again")
